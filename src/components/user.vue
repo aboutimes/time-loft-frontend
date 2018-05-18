9f0d1b43f7c3960311046c687fe0e56e81d24040
aboutimes <aboutime2012@gmail.com>
2018-05-18 15:31:20 +0800

    create user page

---
+++ src/components/user.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="user">
+    user
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'user',
+    data: function () {
+      return {
+
+      }
+    },
+    mounted: function () {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
